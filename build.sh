ae048e6570ed50efd036a53642b9dd2c68090403
KYKY62 <rizkysrg62@gmail.com>
2025-08-28 02:53:10 +0700

    update

--- build.sh
+++ build.sh
@@ -4,16 +4,17 @@ echo "Running build script..."
 
 echo "[1/4] Pulling from github"
 git pull origin 
-git config --global --add safe.directory "${pwd}"
+git config --global --add safe.directory
 
 echo "[2/4] migrate database if one isnt found"
 php artisan migrate
 
 echo "[3/4] installing packages use composer"
 composer install
+php8.3 $(which composer) install 
 
 echo "[4/4] setting up npm"
-npm run intall
+npm run install
 npm run build
 
 echo "Build script complete."
