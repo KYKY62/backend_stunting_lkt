6369acb946e2524ec58fced0c0f63bfe821b7a15
KYKY62 <rizkysrg62@gmail.com>
2025-08-28 03:40:30 +0700

    update last?

--- build.sh
+++ build.sh
@@ -2,20 +2,20 @@
 
 echo "Running build script..."
 
-echo "[1/6] Pulling from github"
+echo "[1/5] Pulling from github"
 git config --global --add safe.directory "${pwd}"
 git pull origin 
 
-echo "[3/6] ubah ke devtest"
-su devtest
+echo "[2/5] ubah ke devtest"
+sudo -u devtest -H bash -lc "cd /home/devtest/public_html && composer install"
 
-echo "[3/6] installing packages use composer"
+echo "[3/5] installing packages use composer"
 composer install
 
-echo "[5/6] migrate database if one isnt found"
+echo "[4/5] migrate database if one isnt found"
 php artisan migrate
 
-echo "[6/6] setting up npm"
+echo "[5/5] setting up npm"
 npm run install
 npm run build
 
